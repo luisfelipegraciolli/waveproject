8b35c704edc62cbd7d7826fa50041bf939a5b485
Breno Pinna <brenopinna1008@gmail.com>
2024-05-09 20:22:32 -0300

    :sparkles: feat: adiciona aviso abaixo do input invalido e incrementa validação

--- login/login.js
+++ login/login.js
@@ -1,18 +1,39 @@
+const formLogin = document.getElementById("form-principal")
+const usuarioInput = document.getElementById("usuario-input")
+const senhaInput = document.getElementById("senha-input")
+const botaoLogin = document.getElementById("botao-login")
+
+formLogin.addEventListener("submit", (e) => {
+  e.preventDefault() //* impede o reload da pagina
+  e.target.reset() //*apaga todos os campos do form
+})
+
+botaoLogin.addEventListener("click", login)
+
 function login() {
   // TODO: Arrumar uma forma de encriptar essas infos para primeiro acesso
   const usuario = "isa123"
   const senha = "123"
 
-  const usuarioInput = document.getElementById("usuario-input").value
-  const senhaInput = document.getElementById("senha-input").value
+  if (!validaInput(usuarioInput, usuario) | !validaInput(senhaInput, senha)) {
+    alert("Usuario ou senha incorretos, tente novamente!")
+    return //*esse return eh pra nao continuar a funcao se der invalido.
+  }
 
-  if (usuarioInput.trim() === "" || senhaInput.trim() === "") {
-      // TODO: adicionar mensagem de erro abaixo do botao de submit
+  alert("USUARIO LOGADO!")
+}
+
+function validaInput(input, esperado) {
+  const spanErro = input.parentElement.lastElementChild
+  const inputValue = input.value.trim()
+  if (inputValue === "") {
+    spanErro.innerText = "Digite ao menos um caractere!"
+    return false
   } else {
-    if (usuario == usuarioInput && senha == senhaInput) {
-      window.location.assign("../index.html")
-    } else {
-      // TODO: adicionar mensagem de erro abaixo do botao de submit
-    }
+    spanErro.innerText = ""
+  }
+
+  if (inputValue !== esperado) {
+    return false
   }
 }
