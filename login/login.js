416d6e47908e36393bacf5eeaded7859445ef387
Breno Pinna <brenopinna1008@gmail.com>
2024-05-09 20:43:55 -0300

    :bug: fix: conserta validação do form e adiciona primeira senha no localStorage

--- login/login.js
+++ login/login.js
@@ -3,6 +3,14 @@ const usuarioInput = document.getElementById("usuario-input")
 const senhaInput = document.getElementById("senha-input")
 const botaoLogin = document.getElementById("botao-login")
 
+//*cria uma primeira senha e usuario se ja nao existir no local storage
+let dados = JSON.parse(localStorage.getItem("dados"))
+
+if (!dados) {
+  dados = { usuario: "admin", senha: "admin" }
+  localStorage.setItem("dados", JSON.stringify(dados))
+}
+
 formLogin.addEventListener("submit", (e) => {
   e.preventDefault() //* impede o reload da pagina
   e.target.reset() //*apaga todos os campos do form
@@ -10,12 +18,15 @@ formLogin.addEventListener("submit", (e) => {
 
 botaoLogin.addEventListener("click", login)
 
-function login() {
+function login(e) {
   // TODO: Arrumar uma forma de encriptar essas infos para primeiro acesso
-  const usuario = "isa123"
-  const senha = "123"
+  const usuario = dados.usuario
+  const senha = dados.senha
 
   if (!validaInput(usuarioInput, usuario) | !validaInput(senhaInput, senha)) {
+    e.preventDefault() //*nao envia o formulario se nao for valido
+    const form = e.target.parentNode
+    form.reset()
     alert("Usuario ou senha incorretos, tente novamente!")
     return //*esse return eh pra nao continuar a funcao se der invalido.
   }
@@ -36,4 +47,6 @@ function validaInput(input, esperado) {
   if (inputValue !== esperado) {
     return false
   }
+
+  return true
 }
