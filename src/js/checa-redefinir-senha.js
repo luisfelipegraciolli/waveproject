290888e16ec6dd28d556d1cff339458913bce8c6
Breno Pinna <brenopinna1008@gmail.com>
2024-05-23 12:16:08 -0300

    :sparkles: feat: troca login no localStorage pelo login no firebase

--- src/js/checa-redefinir-senha.js
+++ src/js/checa-redefinir-senha.js
@@ -1,8 +1,11 @@
-const pergunta = localStorage.getItem("pergunta-de-seguranca")
+import { getAdminInfo } from "../api/get-admin-info.js"
+
 const logado = JSON.parse(sessionStorage.getItem("logado"))
 
+const { pergunta_de_seguranca } = await getAdminInfo("pergunta_de_seguranca")
+
 if (logado) location.href = "/"
-else if (!pergunta) {
+else if (!pergunta_de_seguranca) {
   alert("Não é possível redefinir sua senha sem uma pergunta de segurança cadastrada!")
   location.href = "/login"
 }
