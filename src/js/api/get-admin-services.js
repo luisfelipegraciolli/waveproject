edbc57171d47d767277b5dc5db9067987c264e74
Breno Pinna <brenopinna1008@gmail.com>
2024-05-28 16:31:29 -0300

    :sparkles: feat: adiciona editar servicos

--- src/js/api/get-admin-services.js
+++ src/js/api/get-admin-services.js
@@ -1,12 +1,19 @@
 import { db } from "../lib/firebase-config.js"
 
-export async function getAdminServices() {
+export async function getAdminServices(id) {
   const servicesRef = db.collection("admin").doc("info").collection("services")
-  const querySnapshot = await servicesRef.get()
-  const services = []
-  querySnapshot.forEach((doc) => {
+  if (!id) {
+    const querySnapshot = await servicesRef.get()
+    const services = []
+    querySnapshot.forEach((doc) => {
+      const { funcionario, cliente, servico, categoria, data_hora } = doc.data()
+      services.push({ funcionario, cliente, servico, categoria, data_hora, id: doc.id })
+    })
+    return services
+  } else {
+    const docRef = servicesRef.doc(id)
+    const doc = await docRef.get()
     const { funcionario, cliente, servico, categoria, data_hora } = doc.data()
-    services.push({ funcionario, cliente, servico, categoria, data_hora, id: doc.id })
-  })
-  return services
+    return { funcionario, cliente, servico, categoria, data_hora, id: doc.id }
+  }
 }
